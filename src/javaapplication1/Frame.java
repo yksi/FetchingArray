c9502b1366f230c804dd55ec42ed4170b5fa4cf9
vitaliy krushelnitskiy <embox@ukr.net>
2014-06-04 04:00:48 +0300

    grand method added

--- src/javaapplication1/Frame.java
+++ src/javaapplication1/Frame.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package javaapplication1;
 
 /**
@@ -12,13 +6,19 @@ package javaapplication1;
  */
 public class Frame extends javax.swing.JFrame{
     Frame(int array[][]){
-        setBounds(50, 50, 300, 300);
-        setVisible(true);
+        grand_frame();
+     
         int height = array.length;
         int width = array[0].length;
     }
     
     void fetch_array(int weight, int height) {
         
+    }
+    
+    void grand_frame() {
+        setBounds(50, 50, 300, 300);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
